5d33f728a0acb27a69edac4a1e1d772b830de8f8
sekosolares <buho@seko.dev>
2023-12-07 00:00:59 -0600

    [Add]: Solution Day1 - part 1

--- CSharp/Day1/Program.cs
+++ CSharp/Day1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    public static int GetNumberFromLine(string line) {
+        string pattern = @"\d";
+        MatchCollection numsMatch = Regex.Matches(line.Trim(), pattern);
+        string compoundNum = String.Concat(numsMatch.First(), numsMatch.Last());
+
+        return Int32.Parse(compoundNum);
+    }
+
+    private static void Main(string[] args) {
+        string[] fileContent = File.ReadAllLines("./files/input-d1.txt");
+        int result = 0;
+
+        foreach (string line in fileContent) {
+            result += GetNumberFromLine(line);
+        }
+
+        Console.WriteLine($"Sum of Callibration Values: {result}");
+        
+    }
+}
